2ed65aa03aae93f7897ac74b144482d3ef2eaad8
beta31 <42404233+beta31@users.noreply.github.com>
2022-10-17 12:04:40 +0530

    Time: 84 ms (5.29%), Space: 13.8 MB (54.73%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,9 +1,17 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        n = len(sentence)
-        if n < 26:
-            return False
-        s = set()
+        # set solution
+        # n = len(sentence)
+        # if n < 26:
+        #     return False
+        # s = set()
+        # for ch in sentence:
+        #     s.add(ch)
+        # return len(s)==26
+        
+        # bit solution
+        chr_present = 0
         for ch in sentence:
-            s.add(ch)
-        return len(s)==26
+            chr_present = chr_present | 1 << ord(ch)-ord('a')
+        
+        return chr_present == ((1<<26)-1)
