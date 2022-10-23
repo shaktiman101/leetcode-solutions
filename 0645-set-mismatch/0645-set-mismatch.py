8f560862db392104c96b3da0f3614442172ef25e
beta31 <42404233+beta31@users.noreply.github.com>
2022-10-23 16:14:31 +0530

    Time: 194 ms (97.47%), Space: 15.9 MB (23.96%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -3,14 +3,20 @@ class Solution:
         n = len(nums)
         actual_sum = sum(nums)
         orig_sum = (n*(n+1))//2
-        
-        nums = sorted(nums)
         res = []
-        for i in range(n-1):
-            if nums[i] == nums[i+1]:
-                res.append(nums[i])
-                break
-        res.append(orig_sum+res[0]-actual_sum)
+        
+        # nums = sorted(nums)
+        # for i in range(n-1):
+        #     if nums[i] == nums[i+1]:
+        #         res.append(nums[i])
+        #         break
+        # res.append(orig_sum+res[0]-actual_sum)
+        # return res
+    
+        nums2 = set(nums)
+        set_sum = sum(nums2)
+        res.append(actual_sum-set_sum)
+        res.append(orig_sum-set_sum)
         return res
         
             
