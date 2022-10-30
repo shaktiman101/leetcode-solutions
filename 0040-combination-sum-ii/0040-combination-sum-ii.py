953252473e78626860549ea9ea116ce6380227b3
beta31 <42404233+beta31@users.noreply.github.com>
2022-10-30 11:36:47 +0530

    Time: 92 ms (80.00%), Space: 13.9 MB (60.07%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -24,24 +24,47 @@ class Solution:
 #         backtrack(0, 0)
 #         return ans
 
-        candidates = sorted(candidates)
-        res = set()
-        i, n = 0, len(candidates)
-        def solve(i, target, tmp):
-            if target == 0:
-                res.add(tuple(tmp))
-            if target < 0 or i >= n:
-                return
+#         candidates = sorted(candidates)                 # TC: O(nlog(n))
+#         res = []
+#         i, n = 0, len(candidates)
+#         def solve(i, target, tmp):                      # TC: O(2**n)
+#             if target == 0:
+#                 res.append(tmp.copy())                  # TC: O(k)   -> k = avg. size of tmp list
+#             if target < 0 or i >= n:
+#                 return
             
-            target -= candidates[i]
-            tmp.append(candidates[i])
-            solve(i+1, target, tmp)
+#             target -= candidates[i]
+#             tmp.append(candidates[i])
+#             solve(i+1, target, tmp)
             
-            while i < n-1 and candidates[i] == candidates[i+1]:
-                i += 1
-            target += tmp.pop() #candidates[i]
+#             while i < n-1 and \                         # TC: O(t) -> t = avg. duplicate elements
+#             candidates[i] == candidates[i+1]:     
+#                 i += 1
+#             target += tmp.pop() #candidates[i]
             
-            solve(i+1, target, tmp)
+#             solve(i+1, target, tmp)
 
+#         solve(0, target, [])
+#         return res
+    
+#         # overall TC: O(nlogn) + O(2**n *(k+t))
+
+        candidates = sorted(candidates)
+        res, n = [], len(candidates)
+        
+        def solve(idx, target, tmp):
+            if target == 0:
+                res.append(tmp)
+            # if target < 0 or i >= n or candidates[i] > target:
+            #     return
+            
+            for i in range(idx, n):
+                if i > idx and candidates[i] == candidates[i-1]:
+                    continue
+                if candidates[i] > target:
+                    break
+                solve(i+1, target-candidates[i], tmp+[candidates[i]])
+            
         solve(0, target, [])
         return res
+        
