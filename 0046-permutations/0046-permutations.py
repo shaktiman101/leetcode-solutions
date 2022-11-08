224a6aeced7bb025bebbdd1c6f146c7aaaccc42b
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-11-08 18:53:36 +0530

    Time: 45 ms (89.01%), Space: 14.2 MB (23.76%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,17 +3,30 @@ class Solution:
         n = len(nums)
         res = []
         
-        def solve(nums, tmp, visited):
-            if len(visited) == n:
-                res.append(tmp)
+#         def solve(nums, tmp, visited):
+#             if len(visited) == n:
+#                 res.append(tmp)
+#                 return
+            
+#             for i in range(n):
+#                 if i not in visited:
+#                     visited.add(i)
+#                     solve(nums, tmp+[nums[i]], visited)
+#                     visited.remove(i)
+        
+#         solve(nums, [], set())
+#         return res
+
+        def solve2(nums, idx):
+            if idx == n:
+                res.append(nums.copy())
                 return
             
-            for i in range(n):
-                if i not in visited:
-                    visited.add(i)
-                    solve(nums, tmp+[nums[i]], visited)
-                    visited.remove(i)
+            for i in range(idx, n):
+                nums[idx], nums[i] = nums[i], nums[idx]
+                solve2(nums, idx+1)
+                nums[idx], nums[i] = nums[i], nums[idx]
         
-        solve(nums, [], set())
+        solve2(nums, 0)
         return res
                 
