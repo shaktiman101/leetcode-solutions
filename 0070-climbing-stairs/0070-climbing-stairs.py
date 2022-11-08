50b5d6849c4ca8d43ffccb3154f5e199951d4511
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-11-08 21:04:13 +0530

    Time: 57 ms (42.14%), Space: 13.8 MB (57.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        dp = [-1]*(n+1)
+        def solve(n):
+            if n <= 1:
+                return 1
+            if dp[n] != -1:
+                return dp[n]
+            dp[n] = solve(n-1)+solve(n-2)
+            return dp[n]
+        return solve(n)
+        
 #         n1, n2 = 1, 2
 #         if n < 2:
 #             return n1
@@ -9,16 +19,16 @@ class Solution:
 #             n2 = n1+n2
 #             n1 = tmp
 #         return n2
-        mem = {}
-        def solve(n):
-            if n <= 2:
-                return n
-            if mem.get(n, None):
-                return mem.get(n)
-            n1 = solve(n-1)
-            mem[n-1] = n1
-            n2 = solve(n-2)
-            mem[n-2] = n2
-            return n1 + n2
+#         mem = {}
+#         def solve(n):
+#             if n <= 2:
+#                 return n
+#             if mem.get(n, None):
+#                 return mem.get(n)
+#             n1 = solve(n-1)
+#             mem[n-1] = n1
+#             n2 = solve(n-2)
+#             mem[n-2] = n2
+#             return n1 + n2
         
-        return solve(n)
+#         return solve(n)
