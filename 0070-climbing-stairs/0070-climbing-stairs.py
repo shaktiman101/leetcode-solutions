9f86672d76368a787a00fa96513cf1a26b6d63fa
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-11-09 10:46:16 +0530

    Time: 55 ms (49.96%), Space: 13.9 MB (57.41%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,5 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        # bottom up
+        dp = [-1]*(n+1)
+        dp[0] = dp[1] = 1
+        
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+    
+        # memoization - top down
         dp = [-1]*(n+1)
         def solve(n):
             if n <= 1:
