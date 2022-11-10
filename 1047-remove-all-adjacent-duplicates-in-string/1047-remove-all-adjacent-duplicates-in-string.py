9f6eb8f3cb299bf8146bec6de6ad92776c9b995b
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-11-10 10:31:24 +0530

    Time: 65 ms (98.82%), Space: 14.9 MB (52.58%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -1,13 +1,21 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        n, i = len(s), 1
-        
-        while i < n:
-            if s[i] == s[i-1]:
-                s = s[:i-1]+s[i+1:]
-                n = len(s)
-                i -= 1
-                i = max(1, i)
+        ans = []
+        for ch in s:
+            if ans and ans[-1] == ch:
+                ans.pop()
             else:
-                i += 1
-        return s
+                ans.append(ch)
+        return "".join(ans)
+    
+#         n, i = len(s), 1
+        
+#         while i < n:
+#             if s[i] == s[i-1]:
+#                 s = s[:i-1]+s[i+1:]
+#                 n = len(s)
+#                 i -= 1
+#                 i = max(1, i)
+#             else:
+#                 i += 1
+#         return s
