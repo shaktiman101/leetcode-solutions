2c5f9893b6da90a3f69f36843cfb61390379efbc
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-11-18 18:16:55 +0530

    Time: 38 ms (87.43%), Space: 13.9 MB (12.22%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,8 +1,13 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        # if n == 1:
-        #     return True
+        if n == 0:
+            return False
         prime = [2, 3, 5]
+        
+        for p in prime:
+            while n%p == 0:
+                n /= p
+        return n==1
         
         while n > 1:
             flag = 0
