5e4d8c8fbd59d9ca0f1750fa731a49722f572d3f
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-11-23 21:21:21 +0530

    Time: 87 ms (49.30%), Space: 14.1 MB (20.32%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -15,12 +15,11 @@ class Solution:
             dp[0][j] = 1
                 
         for i in range(1, m):
-            for j in range(1, n):                    
-                w1, w2 = 0, 0
+            for j in range(1, n):
                 if obstacleGrid[i][j] == 0:
                     w1 = dp[i-1][j]
                     w2 = dp[i][j-1]
-                dp[i][j] = w1 + w2
+                    dp[i][j] = w1 + w2
         return dp[m-1][n-1]
     
     
