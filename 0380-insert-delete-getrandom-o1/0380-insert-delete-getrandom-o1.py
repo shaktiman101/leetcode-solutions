bc48da34606691e17f838ad5c7b2b8e4650c90c0
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-11-29 16:12:41 +0530

    Time: 766 ms (70.78%), Space: 61.3 MB (57.51%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,29 +1,48 @@
-import numpy as np
+import random
+from collections import defaultdict
 class RandomizedSet:
 
     def __init__(self):
-        self.s = set()
+        # self.s = set()
+        self.d = defaultdict(int)
         self.arr = []
         self.length = 0
 
     def insert(self, val: int) -> bool:
-        if val in self.s:
+        # if val in self.s:
+        #     return False
+        # self.s.add(val)
+        # self.arr.append(val)
+        # self.length += 1
+        # return True
+        if val in self.d:
             return False
-        self.s.add(val)
+        self.d[val] = self.length
         self.arr.append(val)
         self.length += 1
         return True
 
     def remove(self, val: int) -> bool:
-        if val in self.s:
-            self.s.remove(val)
-            return True
-        return False
+        # if val in self.s:
+        #     self.s.remove(val)
+        #     return True
+        # return False
+        if val not in self.d:
+            return False
+        idx = self.d[val]
+        self.arr[idx], self.arr[-1] = self.arr[-1], self.arr[idx]
+        self.d[self.arr[idx]] = idx
+        self.arr.pop()
+        self.d.pop(val)
+        self.length -= 1
+        return True
 
     def getRandom(self) -> int:
-        idx = np.random.choice(self.length)
-        while self.arr[idx] not in self.s:
-            idx = np.random.choice(self.length)
+        # idx = np.random.choice(self.length)
+        # while self.arr[idx] not in self.s:
+        #     idx = np.random.choice(self.length)
+        # return self.arr[idx]
+        idx = int(random.random() * self.length)
         return self.arr[idx]
 
 
