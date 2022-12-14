862df597a22ef1ea774a67e8a2f7b4fe69b7b3c2
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-12-14 19:19:04 +0530

    Time: 152 ms (86.80%), Space: 14.8 MB (40.88%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,24 +1,5 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-#         A = matrix
-#         for i in range(1,len(A)):
-#             for j in range(len(A[0])):
-
-#                 #edge cases are first column and last column which only have two paths from above
-#                 if j == 0:
-#                     A[i][j]  = min((A[i][j] + A[i - 1][j]), (A[i][j] + A[i - 1][j + 1]) )
-
-#                 elif (j == len(A[0]) - 1):
-#                     A[i][j]  = min((A[i][j] + A[i - 1][j]), (A[i][j] + A[i - 1][j - 1]) )
-
-#                 #every other column will have three paths coming from above
-#                 else:
-#                     A[i][j] = min(A[i][j] + A[i - 1][j],A[i][j] + A[i - 1][j + 1], A[i][j] + A[i - 1][j - 1])
-
-#         # Now that minimum falling sums for each value at the bottom row have been computer
-#         # We can just take the min of the bottow row to get the smallest overall path sum 
-#         return min(A[len(A) - 1])
-
         # bottom-up: tabulation
         n = len(matrix)
         dp = [[0]*n for _ in range(n)]
