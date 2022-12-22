3418543c81e25799386f8414713d4d4e7d03a657
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-12-22 14:10:52 +0530

    Time: 325 ms (66.19%), Space: 14.2 MB (97.31%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,25 +1,25 @@
 from collections import deque
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n, count = len(isConnected), 0
-        visited = [False]*n
-        def dfs(i):
-            visited[i] = True
-            for j in range(n):
-                if isConnected[i][j] == 1 and not visited[j]:
-                    dfs(j)
+#         n, count = len(isConnected), 0
+#         visited = [False]*n
+#         def dfs(i):
+#             visited[i] = True
+#             for j in range(n):
+#                 if isConnected[i][j] == 1 and not visited[j]:
+#                     dfs(j)
         
-        for i in range(n):
-            if not visited[i]:
-                count += 1
-                dfs(i)
-        return count
+#         for i in range(n):
+#             if not visited[i]:
+#                 count += 1
+#                 dfs(i)
+#         return count
             
         n, count = len(isConnected), 0
         visited = [False]*n
         
         for i in range(n):
-            if not visited[i]:
+            if isConnected[i][i] != 2:
                 count += 1
                 # q = deque([i])
                 stack = [i]
@@ -27,9 +27,11 @@ class Solution:
                 while stack:
                     # city = q.popleft()
                     city = stack.pop()
+                    for j in range(n):
+                        isConnected[j][city] = 2
                     visited[city] = True
                     for adj_city in range(n):
-                        if isConnected[city][adj_city] == 1 and not visited[adj_city]:
+                        if isConnected[city][adj_city] == 1: # and not visited[adj_city]:
                             # q.append(adj_city)
                             stack.append(adj_city)
         return count
