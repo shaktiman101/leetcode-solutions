9932ed2b86e19d3f6cb881770c3fa9827a76f780
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-12-26 20:49:20 +0530

    Time: 535 ms (80.17%), Space: 15.2 MB (49.64%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,46 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_index_reachable = 0
+        for curr_idx, max_jump_allowed in enumerate(nums):
+            # if 
+            if curr_idx > max_index_reachable:
+                return False
+            max_index_reachable = max(max_index_reachable, curr_idx+max_jump_allowed)
+        return True
+    
+    
+        # memoization
+        n = len(nums)
+        dp = [-1]*n
+        def func(i):
+            if i >= n:
+                return False
+            if i == n-1:
+                return True
+            if dp[i] != -1:
+                return dp[i]
+            
+            for j in range(1,nums[i]+1):
+                if func(i+j):
+                    dp[i] = True
+                    return dp[i]
+            dp[i] = False
+            return dp[i]
+            
+        return func(0)
+        
+        
+        # SC: recursive stack space
+        # TC: exponential, 1e5**n
+        n = len(nums)
+        def func(i):
+            if i >= n:
+                return False
+            if i == n-1:
+                return True
+            for j in range(1,nums[i]+1):
+                if func(i+j):
+                    return True
+            return False
+            
+        return func(0)
