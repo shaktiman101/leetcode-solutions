a250d625505145e23c36e251d39b98060ddb3f74
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-12-26 21:00:18 +0530

    Time: 9615 ms (5.01%), Space: 15.4 MB (18.05%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,15 +1,26 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        n = len(nums)
-        max_index_reachable = 0
-        for curr_idx, max_jump_allowed in enumerate(nums):
-            if max_index_reachable >= n-1:
-                return True
-            if curr_idx > max_index_reachable:
-                return False
-            max_index_reachable = max(max_index_reachable, curr_idx+max_jump_allowed)
-        return True
+        # n = len(nums)
+        # max_index_reachable = 0
+        # for curr_idx, max_jump_allowed in enumerate(nums):
+        #     if max_index_reachable >= n-1:
+        #         return True
+        #     if curr_idx > max_index_reachable:
+        #         return False
+        #     max_index_reachable = max(max_index_reachable, curr_idx+max_jump_allowed)
+        # return True
     
+        # tabulation
+        n = len(nums)
+        dp = [False]*n
+        dp[n-1] = True
+        
+        for i in range(n-2, -1, -1):
+            for j in range(1, nums[i]+1):
+                if i+j < n and dp[i+j]:
+                    dp[i] = True
+                    break
+        return dp[0]
     
         # memoization
         n = len(nums)
