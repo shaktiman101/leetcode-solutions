158912e8d6e94500c58b9b0c405490a992e20b10
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2022-12-28 23:55:57 +0530

    Time: 1805 ms (93.65%), Space: 29 MB (9.61%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.py
@@ -2,12 +2,12 @@ import heapq
 import math
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
-        # piles = sorted(piles, reverse=True)
         piles = list(map(lambda x: -x, piles))
         heapq.heapify(piles)
         
         while k:
-            largest = heapq.heappop(piles)*-1
-            heapq.heappush(piles, math.ceil(largest/2)*-1)
+            # largest = heapq.heappop(piles)*-1
+            # heapq.heappush(piles, math.ceil(largest/2)*-1)
+            heapq.heapreplace(piles, piles[0]//2)
             k -= 1
         return abs(sum(piles))
