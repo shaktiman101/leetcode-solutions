9a0b7ad39df59616cb0c781fc21d430190915a1a
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-05-04 12:28:05 +0530

    Time: 2159 ms (14.15%), Space: 31.3 MB (17.80%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,62 +1,13 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        
-        num_set = set([])
-        min_num, max_num = float('inf'), float('-inf')
-        for num in nums:
-            # num_set[num] = num_set.get(num, 0)+1
-            num_set.add(num)
-            if num < min_num:
-                min_num = num
-            if num > max_num:
-                max_num = num
-        
-        ans, count = 0, 0
-        # for i in range(min_num, max_num+1):
-        #     if i in num_set:
-        #         count += 1 #num_set[i]
-        #     else:
-        #         ans = max(ans, count)
-        #         count = 0
-        # return max(ans, count)
-        ans, count = 1, 1
+        num_set = set(nums)
+        res = 0
         for num in nums:
             if num-1 not in num_set:
-                tmp = num+1
-                while tmp in num_set:
+                n = num
+                count = 0
+                while n in num_set:
                     count += 1
-                    tmp += 1
-                else:
-                    ans = max(ans, count)
-                    count = 1
-                
-        return max(ans, count)
-        
-        
-        
-        
-        n = len(nums)
-        if n == 0:
-            return 0
-        
-        ele = set()
-        smallest, largest = nums[0], nums[0]
-        for num in nums:
-            if num < smallest:
-                smallest = num
-            if num > largest:
-                largest = num
-            ele.add(num)
-        
-        lcs, lcs_sub = 0, 0
-        for num in nums:
-            tmp = num
-            while tmp in ele:
-                lcs_sub += 1
-                tmp += 1
-            if lcs_sub > lcs:
-                lcs = lcs_sub
-            lcs_sub = 0
-        return lcs
+                    n += 1
+                res = max(count, res)
+        return res
