d10e73f48f61cb7b630be71fe4b3ad86c59b552c
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-05-15 11:11:22 +0530

    Time: 52 ms (9.25%), Space: 16.4 MB (7.45%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,92 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # bottom-up: TC: O(n); SC: O(1)
         n = len(nums)
-        prev2, prev = 0, nums[0]
-        for i in range(1,n):
-            rob = nums[i]
-            if i > 1:
-                rob += prev2
-            not_rob = prev
-            curr = max(rob, not_rob)
-            prev2, prev = prev, curr
-        return prev
         
-        
-        # bottom-up solution: TC: O(n); SC: O(n) -> to store dp array
-        n = len(nums)
-        dp = [0]*n
-        dp[0] = nums[0]
-        
-        for i in range(1,n):
-            rob = nums[i]
-            if i > 1:
-                rob += dp[i-2]
-            not_rob = dp[i-1]
-            dp[i] = max(rob, not_rob)
-        
-        return dp[n-1]
-        
-        
-        # top-down - memoization: TC: O(n); SC: O(n)(to store dp array) + O(n) recursive stack
-        n = len(nums)
-        dp = [0]*n
-        def solve(idx):
+        # recursive approach
+        # def func(nums, idx):
+        #     if idx < 0:
+        #         return 0
+        #     take = nums[idx] + func(nums, idx-2)
+        #     not_take = func(nums, idx-1)
+        #     return max(take, not_take)
+        # return func(nums, n-1)
+    
+    
+        # memoization
+        dp = [-1]*n
+        def func(nums, idx):
             if idx < 0:
                 return 0
-            if dp[idx]:
+            if dp[idx] != -1:
                 return dp[idx]
-            rob = nums[idx] 
-            if idx > 1:
-                rob += solve(idx-2)
-            not_rob = solve(idx-1)
-            dp[idx] = max(rob, not_rob)
+            take = nums[idx] + func(nums, idx-2)
+            not_take = func(nums, idx-1)
+            dp[idx] = max(take, not_take)
             return dp[idx]
         
-        return solve(n-1)
-    
-    
-        # recursive solution: TC: O(2**n); SC: O(n) recursive stack
-        n = len(nums)
-        def solve(idx):
-            # if idx < 0:
-            #     return 0
-            if idx == 0:
-                return nums[0]
-            rob = nums[idx] 
-            if idx > 1:
-                rob += solve(idx-2)
-            not_rob = solve(idx-1)
-            return max(rob, not_rob)
-        return solve(n-1)
-        
-        # n = len(nums)
-        # if n == 1:
-        #     return nums[0]
-        
-#         # recursive + memoization approach
-#         memo = [None]*n
-#         def solve(nums, n):
-#             if n < 0:
-#                 return 0
+        return func(nums, n-1)
+        # return dp[]
             
-#             if memo[n] != None:
-#                 return memo[n]
-                
-#             rob = nums[n] + solve(nums, n-2)
-#             dont_rob = solve(nums, n-1)
-                
-#             memo[n] = max(rob, dont_rob)
-#             return memo[n]
-        
-#         return solve(nums, n-1)
-
-        # iterative + memoization approach
-#         memo = [0, nums[0]]
-#         for i in range(1, n):
-#             memo.append(max(memo[i], memo[i-1] + nums[i]))
             
-#         return memo[-1]
-    
-        
-        
+            
+            
