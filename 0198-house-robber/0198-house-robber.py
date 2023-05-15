8227d6b80fb330fd38da071898a042510199d5e4
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-05-15 11:24:52 +0530

    Time: 40 ms (35.04%), Space: 16.2 MB (19.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -13,20 +13,31 @@ class Solution:
     
     
         # memoization
-        dp = [-1]*n
-        def func(nums, idx):
-            if idx < 0:
-                return 0
-            if dp[idx] != -1:
-                return dp[idx]
-            take = nums[idx] + func(nums, idx-2)
-            not_take = func(nums, idx-1)
-            dp[idx] = max(take, not_take)
-            return dp[idx]
+        # dp = [-1]*n
+#         def func(nums, idx):
+#             if idx < 0:
+#                 return 0
+#             if dp[idx] != -1:
+#                 return dp[idx]
+#             take = nums[idx] + func(nums, idx-2)
+#             not_take = func(nums, idx-1)
+#             dp[idx] = max(take, not_take)
+#             return dp[idx]
         
-        return func(nums, n-1)
-        # return dp[]
-            
+#         return func(nums, n-1)
+        
+        
+        #tabulation
+        dp = [0]*n
+        dp[0] = nums[0]
+        for idx in range(1, n):
+            take = nums[idx]
+            if idx > 1:
+                take += dp[idx-2]
+            not_take = dp[idx-1]
+            dp[idx] = max(take, not_take)
+        
+        return dp[-1]
             
             
             
