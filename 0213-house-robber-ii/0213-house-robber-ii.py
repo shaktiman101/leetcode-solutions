a33a057cf3b0ab280e93ff3d9c5209019b02bf91
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-05-15 12:19:40 +0530

    Time: 53 ms (7.98%), Space: 16.4 MB (8.29%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -17,20 +17,35 @@ class Solution:
         # return max(first_skip, last_skip)
     
         # memoization
-        def func(nums, idx):
-            if idx < 0:
-                return 0
-            if dp[idx] != -1:
-                return dp[idx]
-            take = nums[idx] + func(nums, idx-2)
-            not_take = func(nums, idx-1)
-            dp[idx] = max(take, not_take)
-            return dp[idx]
+#         def func(nums, idx):
+#             if idx < 0:
+#                 return 0
+#             if dp[idx] != -1:
+#                 return dp[idx]
+#             take = nums[idx] + func(nums, idx-2)
+#             not_take = func(nums, idx-1)
+#             dp[idx] = max(take, not_take)
+#             return dp[idx]
             
-        dp = [-1]*n
-        first_skip = func(nums[1:], n-2)
+#         dp = [-1]*n
+#         first_skip = func(nums[1:], n-2)
         
-        dp = [-1]*n
-        last_skip = func(nums[:-1], n-2)
-        return max(first_skip, last_skip)
+#         dp = [-1]*n
+#         last_skip = func(nums[:-1], n-2)
+#         return max(first_skip, last_skip)
+    
+    
+        # tabulation
+        def func(nums):
+            n = len(nums)
+            dp = [0]*n
+            dp[0] = nums[0]
+            for idx in range(1, n):
+                take = nums[idx] 
+                if idx > 1:
+                    take += dp[idx-2]
+                not_take = dp[idx-1]
+                dp[idx] = max(take, not_take)
+            return dp[-1]
+        return max(func(nums[1:]), func(nums[:-1]))
         
