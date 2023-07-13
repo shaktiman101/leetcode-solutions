66da211401acafd314d3bdfb213f1391b096a180
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-13 22:28:51 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.py
+++ Topological sort - GFG/topological-sort.py
@@ -1,24 +1,48 @@
+from collections import deque
 class Solution:
     
     #Function to return list containing vertices in Topological order.
     def topoSort(self, V, adj):
-        visited = [False]*V
-        stack = []
+        # # DFS approach
+        # visited = [False]*V
+        # stack = []
         
-        def dfs(node):
-            visited[node] = True
-            for adj_node in adj[node]:
-                if not visited[adj_node]:
-                    dfs(adj_node)
-            stack.append(node)
-            # print(stack)
+        # def dfs(node):
+        #     visited[node] = True
+        #     for adj_node in adj[node]:
+        #         if not visited[adj_node]:
+        #             dfs(adj_node)
+        #     stack.append(node)
             
-        for i in range(V):
-            if not visited[i]:
-                dfs(i)
-        topo_sort = stack[::-1]
-        # print(topo_sort)
+        # for i in range(V):
+        #     if not visited[i]:
+        #         dfs(i)
+        # topo_sort = stack[::-1]
+        # return topo_sort
+        
+        queue = deque([])
+        in_degree = [0]*V
+        
+        for node in range(V):
+            for adj_node in adj[node]:
+                in_degree[adj_node] += 1
+                
+        for node, deg in enumerate(in_degree):
+            if deg == 0:
+                queue.append(node)
+           
+        topo_sort = []     
+        while queue:
+            node = queue.popleft()
+            topo_sort.append(node)
+            
+            for adj_node in adj[node]:
+                in_degree[adj_node] -= 1
+                if in_degree[adj_node] == 0:
+                    queue.append(adj_node)
+            
         return topo_sort
+        
 #{ 
  # Driver Code Starts
 # Driver Program
