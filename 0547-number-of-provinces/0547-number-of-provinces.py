9316da86ec117aa44367d1679bce58658187cb49
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-13 13:29:36 +0530

    Time: 253 ms (11.82%), Space: 17.8 MB (7.70%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,38 +1,54 @@
-from collections import deque
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-#         n, count = len(isConnected), 0
-#         visited = [False]*n
-#         def dfs(i):
-#             visited[i] = True
-#             for j in range(n):
-#                 if isConnected[i][j] == 1 and not visited[j]:
-#                     dfs(j)
+        V = len(isConnected)
+        edges = []
+        # for node in range(V):
+        #     for adj_node, is_connected  in enumerate(adj[node]):
+        #         if is_connected and node != adj_node:
+        #             edges.append((node, adj_node))
+        for u in range(V):
+            for v in range(V):
+                if isConnected[u][v] and u != v:
+                    edges.append((u,v))
+                    
+        print(edges)
+        parent= [i for i in range(V)]
+        size = [1 for _ in range(V)]
         
-#         for i in range(n):
-#             if not visited[i]:
-#                 count += 1
-#                 dfs(i)
-#         return count
+        def find_uparent(node):
+            if node == parent[node]:
+                return node
+            parent[node] = find_uparent(parent[node])
+            return parent[node]
             
-        n, count = len(isConnected), 0
-        visited = [False]*n
-        
-        for i in range(n):
-            if isConnected[i][i] != 2:
-                count += 1
-                # q = deque([i])
-                stack = [i]
-                
-                while stack:
-                    # city = q.popleft()
-                    city = stack.pop()
-                    for j in range(n):
-                        isConnected[j][city] = 2
-                    visited[city] = True
-                    for adj_city in range(n):
-                        if isConnected[city][adj_city] == 1: # and not visited[adj_city]:
-                            # q.append(adj_city)
-                            stack.append(adj_city)
-        return count
+        def union(u, v):
+            pu = find_uparent(u)
+            pv = find_uparent(v)
+            if pu == pv:
+                return
+            
+            if size[pu] > size[pv]:
+                parent[pv] = pu
+                size[pu] += size[pv]
+            else:
+                parent[pu] = pv
+                size[pv] += size[pu]
+            
+        for u, v in edges:
+            union(u, v)
+            
+        for node in range(V):       # to compress any remaining edges
+            find_uparent(node)
+            
+        # provinces = 0
+        # provinces_name= []
+        # for i in range(V):
+        #     if i == parent[i]:
+        #         provinces += 1
+        #         provinces_name.append(i)
+        # print(set(parent))
+        # print(provinces_name)
+        # print(parent)
+        # print(size)
+        return len(set(parent))
         
