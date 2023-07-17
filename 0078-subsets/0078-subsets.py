82480ca8e6ecde256b035c2a7dbe99413970ee42
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-17 22:03:39 +0530

    Time: 42 ms (92.26%), Space: 16.4 MB (80.99%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,49 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums)
-        ans = []
+        power_set = []
         
-        def solve(tmp, n):
-            if n < 0:
-                return
-            if n == 0:
-                ans.append(tmp.copy())
-            solve(tmp+[nums[n-1]], n-1)
-            solve(tmp, n-1)
-        
-        solve([], n)
-        return ans
-    
-        # n = len(nums)
-        # ans = [[]]
-        # for i in range(n):
-        #     tmp = []
-        #     for subset in ans:
-        #         subset2 = subset.copy()
-        #         subset2.append(nums[i])
-        #         tmp.append(subset2)
-        #     ans.extend(tmp)
-        # return ans
-    
-        ans = []
-        subset = []
-        
-        def dfs(i):
-            if i >= len(nums):
-                ans.append(subset.copy())
+        def func(idx, subset):
+            if idx < 0:
+                power_set.append(subset.copy())
                 return
             
-            # decision to select ith index element in subet
-            subset.append(nums[i])
-            dfs(i+1)
+            subset.append(nums[idx])
+            func(idx-1, subset)
             
-            # decision to NOT select the ith index element
             subset.pop()
-            dfs(i+1)
-            
+            func(idx-1, subset)
         
-        # starting index of nums
-        dfs(0)
-        return ans
+        func(len(nums)-1, [])
+        return power_set
         
-            
