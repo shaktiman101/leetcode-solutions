ed1996d3ac83ca468241d203a1637a13f84d2a3b
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-20 18:53:15 +0530

    Time: 78 ms (58.19%), Space: 16.4 MB (58.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,43 +1,24 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-#         ans = []
-#         subset = []
-        
-#         def dfs(i, target):
-#             if i >= len(candidates) or target < 0:
-#                 return
-#             if target == 0:
-#                 ans.append(subset.copy())
-#                 return
-            
-#             subset.append(candidates[i])
-#             target -= candidates[i]
-#             dfs(i, target)
-            
-#             tmp = subset.pop()
-#             target += tmp
-#             dfs(i+1, target)
-            
-#         dfs(0, target)
-#         return ans
-
         res = []
-        def solve(n, tmp, sum_):
-            
-            if sum_ == target:
+        
+        def unique_comb(idx, k, tmp):
+            if idx < 0 or k > target:
+                return
+            if k == target:
                 res.append(tmp.copy())
                 return
-            if sum_ > target:
-                return
-            if n<1:
-                return
-            tmp.append(candidates[n-1])
-            sum_ += candidates[n-1]
-            solve(n, tmp, sum_)
+                
+            # take
+            k += candidates[idx]
+            tmp.append(candidates[idx])
+            unique_comb(idx, k, tmp)
             
-            tmp.remove(candidates[n-1])
-            sum_ -= candidates[n-1]
-            solve(n-1, tmp, sum_)
-            
-        solve(len(candidates), [], 0)
+            # not-take
+            k -= candidates[idx]
+            tmp.pop()
+            unique_comb(idx-1, k, tmp)
+        
+        unique_comb(len(candidates)-1, 0, [])
         return res
+        
