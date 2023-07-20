cd63b1259ae00277a5e63c24311b888787f9afd6
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-20 19:17:44 +0530

    Time: 61 ms (94.95%), Space: 16.6 MB (25.50%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,23 +2,47 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         
-        def unique_comb(idx, k, tmp):
-            if idx < 0 or k > target:
-                return
-            if k == target:
-                res.append(tmp.copy())
+#         def unique_comb(idx, k, tmp):
+#             if idx < 0 or k > target:
+#                 return
+#             if k == target:
+#                 res.append(tmp.copy())
+#                 return
+                
+#             # take
+#             k += candidates[idx]
+#             tmp.append(candidates[idx])
+#             unique_comb(idx, k, tmp)
+            
+#             # not-take
+#             k -= candidates[idx]
+#             tmp.pop()
+#             unique_comb(idx-1, k, tmp)
+        
+#         unique_comb(len(candidates)-1, 0, [])
+#         return res
+
+        def unique_comb(idx, target, tmp):
+            if idx < 0:
+                if target == 0:
+                    res.append(tmp.copy())
                 return
                 
             # take
-            k += candidates[idx]
-            tmp.append(candidates[idx])
-            unique_comb(idx, k, tmp)
+            if candidates[idx] <= target:
+                target -= candidates[idx]
+                tmp.append(candidates[idx])
+                unique_comb(idx, target, tmp)
+                
+                # not-take
+                target += candidates[idx]
+                tmp.pop()
+                unique_comb(idx-1, target, tmp)
+            else:
+                unique_comb(idx-1, target, tmp)
             
-            # not-take
-            k -= candidates[idx]
-            tmp.pop()
-            unique_comb(idx-1, k, tmp)
+            
         
-        unique_comb(len(candidates)-1, 0, [])
+        unique_comb(len(candidates)-1, target, [])
         return res
         
