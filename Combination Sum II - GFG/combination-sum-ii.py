d2b65d123a557553716f052bce7c44896322eafb
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-23 16:17:25 +0530

    Added solution - LeetHub

--- Combination Sum II - GFG/combination-sum-ii.py
+++ Combination Sum II - GFG/combination-sum-ii.py
@@ -5,24 +5,21 @@ class Solution:
         res = []
         candidates = sorted(candidates, reverse=True)
         
-        def func(idx, target, tmp, tmp_count):
+        def func(idx, target, tmp):
             if idx < 0:
                 if target == 0:
                     res.append(tmp.copy())
                 return
             
-            
             if candidates[idx] <= target:
                     tmp.append(candidates[idx])
-                    tmp_count += 1
-                    func(idx-1, target-candidates[idx], tmp, tmp_count)
+                    func(idx-1, target-candidates[idx], tmp)
                     tmp.pop()
-                    tmp_count -= 1
-            while idx >= 0 and candidates[idx] == candidates[idx-1]:
+            while idx > 0 and candidates[idx] == candidates[idx-1]:
                 idx -= 1
-            func(idx-1, target, tmp, tmp_count)
+            func(idx-1, target, tmp)
         
-        func(len(candidates)-1, target, [], 0)
+        func(len(candidates)-1, target, [])
         return res
 
 
