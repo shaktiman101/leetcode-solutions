4f96f7f8e59780fdd6fb376ee2a7652457293e9d
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-23 16:40:24 +0530

    Added solution - LeetHub

--- Subset II - GFG/subset-ii.py
+++ Subset II - GFG/subset-ii.py
@@ -2,25 +2,40 @@
 
 class Solution:
     def printUniqueSubset(self, nums):
-        nums = sorted(nums)
+        nums = sorted(nums) #, reverse=True)
         n = len(nums)
         res = []
         
-        def func(idx, subset):
-            if idx >= n:
-                res.append(subset.copy())
-                return
+        # def func(idx, subset):
+        #     if idx >= n:
+        #         res.append(subset.copy())
+        #         return
         
-            subset.append(nums[idx])
-            func(idx+1, subset)
-            subset.pop()
+        #     subset.append(nums[idx])
+        #     func(idx+1, subset)
+        #     subset.pop()
+                
+        #     while idx<n-1 and nums[idx]==nums[idx+1]:
+        #         idx += 1
             
-            while idx<n-1 and nums[idx]==nums[idx+1]:
-                idx += 1
-            func(idx+1, subset)
+        #     func(idx+1, subset)    
         
+        # func(0, [])
+        # return sorted(res)
+        
+        def func(idx, subset):
+            res.append(subset.copy())
+            
+            for i in range(idx, n):
+                if i != idx and nums[i] == nums[i-1]:
+                    continue
+                
+                subset.append(nums[i])
+                func(i+1, subset)
+                subset.pop()
+                
         func(0, [])
-        return sorted(res)
+        return res
 
 
 #{ 
