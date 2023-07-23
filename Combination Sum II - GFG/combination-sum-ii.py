eaa46bc03bb00b869432da1648c48cfc0fd59c86
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-23 15:21:32 +0530

    Added solution - LeetHub

---
+++ Combination Sum II - GFG/combination-sum-ii.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+
+class Solution:
+    def combinationSum2(self, candidates, target):
+        res = []
+        candidates = sorted(candidates, reverse=True)
+        
+        def func(idx, target, tmp, tmp_count):
+            if idx < 0:
+                if target == 0:
+                    res.append(tmp.copy())
+                return
+            
+            
+            if candidates[idx] <= target:
+                    tmp.append(candidates[idx])
+                    tmp_count += 1
+                    func(idx-1, target-candidates[idx], tmp, tmp_count)
+                    tmp.pop()
+                    tmp_count -= 1
+            while idx >= 0 and candidates[idx] == candidates[idx-1]:
+                idx -= 1
+            func(idx-1, target, tmp, tmp_count)
+        
+        func(len(candidates)-1, target, [], 0)
+        return res
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        n, target = list(map(int, input().split()))
+        candidates = list(map(int, input().split()))
+        ob = Solution()
+        res = ob.combinationSum2(candidates, target)
+        res.sort()
+        print('[ ', end = '')
+        for subset in res:
+            print('[ ', end = '')
+            for val in subset:
+                print(val, end = ' ')
+            print(']', end = '')
+        print(' ]')
+# } Driver Code Ends
