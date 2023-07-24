e52fd24ef662716de14608fdaa1bd3edc6d80587
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-24 21:11:30 +0530

    Time: 56 ms (56.95%), Space: 16.7 MB (13.55%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,45 +1,23 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        ans = [[]]
-        for i in range(n):
-            tmp = []
-            for subset in ans:
-                for j in range(len(subset)+1):
-                    tmp.append(subset[:j]+[nums[i]]+subset[j:])
-            ans = tmp #.copy()
-        # print(ans)
-        return ans
-                
-        
-        
-#         def solve(nums, tmp, visited):
-#             if len(visited) == n:
-#                 res.append(tmp)
-#                 return
-            
-#             for i in range(n):
-#                 if i not in visited:
-#                     visited.add(i)
-#                     solve(nums, tmp+[nums[i]], visited)
-#                     visited.remove(i)
-        
-#         solve(nums, [], set())
-#         return res
-
-        n = len(nums)
+        visited = [False]*n
         res = []
-
-        def solve2(nums, idx):
-            if idx == n:
-                res.append(nums.copy())
+        
+        def func(tmp_size, tmp, visited):
+            if tmp_size == n:
+                res.append(tmp.copy())
                 return
             
-            for i in range(idx, n):
-                nums[idx], nums[i] = nums[i], nums[idx]
-                solve2(nums, idx+1)
-                nums[idx], nums[i] = nums[i], nums[idx]
+            for i in range(n):
+                if visited[i]:
+                    continue
+                    
+                tmp.append(nums[i])
+                visited[i] = True
+                func(tmp_size+1, tmp, visited)
+                visited[i] = False
+                tmp.pop()
         
-        solve2(nums, 0)
+        func(0, [], visited)
         return res
-                
