a01b320419e54c0d068920761ce288213d148677
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-27 11:44:16 +0530

    Time: 63 ms (15.27%), Space: 16.5 MB (41.76%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -4,20 +4,39 @@ class Solution:
         visited = [False]*n
         res = []
         
-        def func(tmp_size, tmp, visited):
-            if tmp_size == n:
-                res.append(tmp.copy())
-                return
+#         def func(tmp_size, tmp, visited):
+#             if tmp_size == n:
+#                 res.append(tmp.copy())
+#                 return
+            
+#             for i in range(n):
+#                 if visited[i]:
+#                     continue
+                    
+#                 tmp.append(nums[i])
+#                 visited[i] = True
+#                 func(tmp_size+1, tmp, visited)
+#                 visited[i] = False
+#                 tmp.pop()
+        
+#         func(0, [], visited)
+#         return res
+
+        def func(idx, cur):
+            if len(cur) == n:
+                res.append(cur.copy())
+                return 
             
             for i in range(n):
                 if visited[i]:
                     continue
-                    
-                tmp.append(nums[i])
+                
                 visited[i] = True
-                func(tmp_size+1, tmp, visited)
+                cur.append(nums[i])
+                func(i+1, cur)
+                cur.pop()
                 visited[i] = False
-                tmp.pop()
-        
-        func(0, [], visited)
+                
+        func(0, [])
         return res
+                
