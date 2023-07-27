21350c4eea51ecbc95bf6dbaae6b59a58f7b530b
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-27 12:07:14 +0530

    Time: 66 ms (88.52%), Space: 16.5 MB (58.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,6 +1,7 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        n = len(candidates)
         
 #         def unique_comb(idx, k, tmp):
 #             if idx < 0 or k > target:
@@ -22,25 +23,40 @@ class Solution:
 #         unique_comb(len(candidates)-1, 0, [])
 #         return res
 
-        def unique_comb(idx, target, tmp):
-            if idx < 0:
-                if target == 0:
-                    res.append(tmp.copy())
+#         def unique_comb(idx, target, tmp):
+#             if idx < 0:
+#                 if target == 0:
+#                     res.append(tmp.copy())
+#                 return
+                
+#             # take
+#             if candidates[idx] <= target:
+#                 target -= candidates[idx]
+#                 tmp.append(candidates[idx])
+#                 unique_comb(idx, target, tmp)
+#                 target += candidates[idx]
+#                 tmp.pop()
+#             # else:
+#             # not-take
+#             unique_comb(idx-1, target, tmp)
+        
+#         unique_comb(len(candidates)-1, target, [])
+#         return res
+
+        # candidates.sort()
+        def func(idx, tmp, target):
+            if target < 0:
+                return
+            
+            if target == 0:
+                res.append(tmp.copy())
                 return
                 
-            # take
-            if candidates[idx] <= target:
-                target -= candidates[idx]
-                tmp.append(candidates[idx])
-                unique_comb(idx, target, tmp)
-                target += candidates[idx]
+            for i in range(idx, n):
+                tmp.append(candidates[i])
+                func(i, tmp, target-candidates[i])
                 tmp.pop()
-            # else:
-            # not-take
-            unique_comb(idx-1, target, tmp)
-            
-            
-        
-        unique_comb(len(candidates)-1, target, [])
+                
+        func(0, [], target)
         return res
         
