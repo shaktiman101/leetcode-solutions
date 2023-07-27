c505e5dda4eb6b2555ef31be128f2c13d5462e23
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-27 11:18:12 +0530

    Time: 49 ms (63.61%), Space: 16.7 MB (12.87%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,18 +1,30 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         power_set = []
+        n = len(nums)
         
+#         def func(idx, subset):
+#             if idx < 0:
+#                 power_set.append(subset.copy())
+#                 return
+            
+#             subset.append(nums[idx])
+#             func(idx-1, subset)
+            
+#             subset.pop()
+#             func(idx-1, subset)
+        
+#         func(len(nums)-1, [])
+#         return power_set
+
         def func(idx, subset):
-            if idx < 0:
-                power_set.append(subset.copy())
-                return
+            power_set.append(subset.copy())
             
-            subset.append(nums[idx])
-            func(idx-1, subset)
-            
-            subset.pop()
-            func(idx-1, subset)
-        
-        func(len(nums)-1, [])
+            for i in range(idx, n):
+                subset.append(nums[i])
+                func(i+1, subset)
+                subset.pop()
+                
+        func(0, [])
         return power_set
         
