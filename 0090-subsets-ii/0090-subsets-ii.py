565f5c90ed987d6c66c18016d9658cbbd906df15
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-07-27 11:23:51 +0530

    Time: 36 ms (99.59%), Space: 16.7 MB (25.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,73 +1,18 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-#         ans, subset = set(), []
-#         nums.sort()
+        nums.sort()
+        n = len(nums)
+        res = []
         
-#         def dfs(i):
-#             if i >= len(nums):
-#                 if tuple(subset) not in ans:
-#                     ans.add(tuple(subset.copy()))
-#                 return
+        def func(idx, cur):
+            res.append(cur.copy())
             
-#             subset.append(nums[i])
-#             dfs(i+1)
-            
-#             if subset:
-#                 subset.pop()
-#             dfs(i+1)
-        
-#         dfs(0)
-#         return ans
-
-        # second approach
-#         ans, subset = [], []
-#         nums.sort()
-        
-#         def dfs(i):
-#             if i >= len(nums):
-#                 ans.append(subset.copy())
-#                 return
-            
-#             subset.append(nums[i])
-#             dfs(i+1)
-            
-#             subset.pop()
-#             while i+1 < len(nums) and nums[i] == nums[i+1]:
-#                 i += 1
-#             dfs(i+1)
-        
-#         dfs(0)
-#         return ans
-    
-#         ans, subset = [], []
-#         nums.sort()
-        
-#         def dfs(start):
-#             # if start == len(nums):
-#             ans.append(subset[:])
-#                 # return
-            
-#             for i in range(start, len(nums)):
-#                 if i > start and nums[i] == nums[i-1]:
-#                     continue
-#                 subset.append(nums[i])
-#                 dfs(i+1)
-#                 subset.pop()
-        
-#         dfs(0)
-#         return ans
-
-        nums = sorted(nums)
-        res, n = [], len(nums)
-        def solve(idx, subset):
-            # if n < 1:
-            res.append(subset)
-                # return
             for i in range(idx, n):
-                if i > idx and nums[i] == nums[i-1]:
+                if i>idx and nums[i] == nums[i-1]:
                     continue
-                solve(i+1, subset+[nums[i]])
-                
-        solve(0, [])
-        return res
+                cur.append(nums[i])
+                func(i+1, cur)
+                cur.pop()
         
+        func(0, [])
+        return res
