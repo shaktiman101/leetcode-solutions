2300b9108243eabdbc83ffd1004d5651b9998b29
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-08-03 20:48:31 +0530

    Time: 42 ms (80.23%), Space: 16.2 MB (96.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -18,18 +18,28 @@ class Solution:
     
     
         # memoized approach
-        dp = [[-1]*n for _ in range(m)]
-        def func(row, col):
-            if row >= m or col >= n:
-                return 0
-            if row==m-1 and col==n-1:
-                return 1
-            if dp[row][col] != -1:
-                return dp[row][col]
+        # TC: O(m*n)
+        # SC: O(2*m*n)
+# #         dp = [[-1]*n for _ in range(m)]
+# #         def func(row, col):
+# #             if row >= m or col >= n:
+# #                 return 0
+# #             if row==m-1 and col==n-1:
+# #                 return 1
+# #             if dp[row][col] != -1:
+# #                 return dp[row][col]
             
-            col_incr = func(row, col+1)
-            row_incr = func(row+1, col)
-            dp[row][col] = col_incr+row_incr
-            return dp[row][col]
+# #             col_incr = func(row, col+1)
+# #             row_incr = func(row+1, col)
+# #             dp[row][col] = col_incr+row_incr
+# #             return dp[row][col]
         
-        return func(0, 0)
+#         return func(0, 0)
+
+        dp = [[1]*n for _ in range(m)]
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i][j-1]+dp[i-1][j]
+        
+        return dp[-1][-1]
