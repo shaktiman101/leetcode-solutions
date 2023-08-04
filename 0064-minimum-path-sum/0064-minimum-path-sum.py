1c9a1b5e9b1f8ed50ff529848b126c9540ede923
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-08-04 13:11:53 +0530

    Time: 87 ms (98.12%), Space: 17.9 MB (88.69%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -17,18 +17,37 @@ class Solution:
 #         return min_sum(0, 0)
 
         # memoized 
-        dp = [[-1]*n for _ in range(m)]
-        def min_sum(row, col):
-            if row >= m or col >= n:
-                return float('inf')
-            if row == m-1 and col == n-1:
-                return grid[m-1][n-1]
-            if dp[row][col] != -1:
-                return dp[row][col]
+#         dp = [[-1]*n for _ in range(m)]
+#         def min_sum(row, col):
+#             if row >= m or col >= n:
+#                 return float('inf')
+#             if row == m-1 and col == n-1:
+#                 return grid[m-1][n-1]
+#             if dp[row][col] != -1:
+#                 return dp[row][col]
             
-            sum1 = min_sum(row, col+1)
-            sum2 = min_sum(row+1, col)
-            dp[row][col] = grid[row][col] + min(sum1, sum2)
-            return dp[row][col]
+#             sum1 = min_sum(row, col+1)
+#             sum2 = min_sum(row+1, col)
+#             dp[row][col] = grid[row][col] + min(sum1, sum2)
+#             return dp[row][col]
         
-        return min_sum(0, 0)
+#         return min_sum(0, 0)
+    
+        # bottom-up
+        dp = [[0]*n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        
+        for i in range(m):
+            for j in range(n):
+                if i==0 and j==0:
+                    continue
+                if i==0:
+                    dp[0][j] = dp[0][j-1] + grid[i][j]
+                    continue
+                if j==0:
+                    dp[i][0] = dp[i-1][0] + grid[i][j]
+                    continue
+                    
+                dp[i][j] = grid[i][j] + min(dp[i][j-1], dp[i-1][j])
+                
+        return dp[m-1][n-1]
