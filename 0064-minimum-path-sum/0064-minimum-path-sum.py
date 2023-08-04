2951c9f4f5e0e18133c03dbdefca7be96e9b7ab6
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-08-04 13:04:54 +0530

    Time: 106 ms (74.57%), Space: 18.5 MB (19.19%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,42 +1,34 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        #top-down solution
         m, n = len(grid), len(grid[0])
-        dp = [[0]*n for _ in range(m)]
-        dp[0][0] = grid[0][0]
         
-        def solve(row, col):
-            if row < 0 or col < 0:
-                return 0
-            if row==0 and col==0:
-                return grid[0][0]
-            if dp[row][col]:
+        # recursive
+        # TC: 
+#         def min_sum(row, col):
+#             if row >= m or col >= n:
+#                 return float('inf')
+#             if row == m-1 and col == n-1:
+#                 return grid[m-1][n-1]
+            
+#             sum1 = min_sum(row, col+1)
+#             sum2 = min_sum(row+1, col)
+#             return grid[row][col] + min(sum1, sum2)
+        
+#         return min_sum(0, 0)
+
+        # memoized 
+        dp = [[-1]*n for _ in range(m)]
+        def min_sum(row, col):
+            if row >= m or col >= n:
+                return float('inf')
+            if row == m-1 and col == n-1:
+                return grid[m-1][n-1]
+            if dp[row][col] != -1:
                 return dp[row][col]
-            c1, c2 = float('inf'), float('inf')
-            if row > 0:
-                c1 = solve(row-1, col)
-            if col > 0:
-                c2 = solve(row, col-1)
-            dp[row][col] = grid[row][col] + min(c1, c2)
+            
+            sum1 = min_sum(row, col+1)
+            sum2 = min_sum(row+1, col)
+            dp[row][col] = grid[row][col] + min(sum1, sum2)
             return dp[row][col]
-            
-        solve(m-1, n-1)
-        return dp[m-1][n-1]
-    
-        #recusrive solution
-        m, n = len(grid), len(grid[0])
         
-        def solve(row, col):
-            if row < 0 or col < 0:
-                return 0
-            if row==0 and col==0:
-                return grid[0][0]
-            c1, c2 = float('inf'), float('inf')
-            if row > 0:
-                c1 = solve(row-1, col)
-            if col > 0:
-                c2 = solve(row, col-1)
-            return grid[row][col] + min(c1, c2)
-            
-        return solve(m-1, n-1)
-        
+        return min_sum(0, 0)
