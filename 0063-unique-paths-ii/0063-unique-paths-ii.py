da2c9169fe87b32965b40ed019dbc8d4f23307ad
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-08-04 12:07:57 +0530

    Time: 46 ms (94.47%), Space: 16.6 MB (27.60%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,62 +1,45 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        # bottom-up solution
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0]*n for _ in range(m)]
+        if obstacleGrid[0][0] == 1:
+            return 0
+        # recursive approach
+        # TC: O(2*(m*n))
+        # SC: O(m*n)
+#         def unique_paths(row, col):
+#             if row >= m or col >= n:
+#                 return 0
+#             if row == m-1 or col == n-1:
+#                 return 1
+            
+#             col_ways, row_ways = 0, 0
+#             if col < n and obstacleGrid[row][col+1] == 0:
+#                 col_ways = unique_paths(row, col+1)
+#             if row < m and obstacleGrid[row+1][col] == 0:
+#                 row_ways = unique_paths(row+1, col)
+#             return col_ways+row_ways
         
-        for i in range(m):
-            if obstacleGrid[i][0] == 1:
-                break
-            dp[i][0] = 1
-        
-        for j in range(n):
-            if obstacleGrid[0][j] == 1:
-                break
-            dp[0][j] = 1
-                
-        for i in range(1, m):
-            for j in range(1, n):
-                if obstacleGrid[i][j] == 0:
-                    w1 = dp[i-1][j]
-                    w2 = dp[i][j-1]
-                    dp[i][j] = w1 + w2
-        return dp[m-1][n-1]
-    
-    
-        # top-down solution
-        m, n = len(obstacleGrid), len(obstacleGrid[0])        
-        dp = [[0]*n for _ in range(m)]
-        dp[0][0] = 1
-        def solve(row, col):
-            if row < 0 or col < 0:
+#         return unique_paths(0, 0)
+
+
+        # memoized approach
+        dp = [[-1]*n for _ in range(m)]
+        def unique_paths(row, col):
+            if row >= m or col >= n:
                 return 0
-            if obstacleGrid[row][col] == 1:
-                return 0
-            if row == 0 and col == 0:
+            if row == m-1 and col == n-1:
                 return 1
-            if dp[row][col]:
+            if dp[row][col] != -1:
                 return dp[row][col]
-            w1 = solve(row-1, col)
-            w2 = solve(row, col-1)
-            dp[row][col] = w1+w2
+            
+            col_ways, row_ways = 0, 0
+            if col+1 < n and obstacleGrid[row][col+1] == 0:
+                col_ways = unique_paths(row, col+1)
+            if row+1 < m and obstacleGrid[row+1][col] == 0:
+                row_ways = unique_paths(row+1, col)
+            dp[row][col] = col_ways+row_ways
             return dp[row][col]
         
-        return solve(m-1, n-1)
-    
+        return unique_paths(0, 0)
         
-        # recursive solution
-        m, n = len(obstacleGrid), len(obstacleGrid[0])        
-        def solve(row, col):
-            if row < 0 or col < 0:
-                return 0
-            if row == 0 and col == 0:
-                return 1
-            w1 = 0
-            if obstacleGrid[row-1][col] == 0:
-                w1 = solve(row-1, col)
-            w2 = 0
-            if obstacleGrid[row][col-1] == 0:
-                w2 = solve(row, col-1)
-            return w1+w2
         
-        return solve(m-1, n-1)
