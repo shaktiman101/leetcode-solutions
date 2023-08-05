92974e770a87c60a0e16c3a1c908a697440c7bdd
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-08-05 09:18:22 +0530

    Time: 59 ms (99.00%), Space: 17.9 MB (20.74%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,41 +1,33 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # bottom-up solution
         n = len(triangle)
-        dp = [[0]*(i+1) for i in range(n)]
-        dp[0][0] = triangle[0][0]
         
-        for i in range(1, n):
-            for j in range(len(triangle[i])):
-                c1 = float('inf')
-                if j > 0:
-                    c1 = dp[i-1][j-1]
-                c2 = float('inf')
-                if j < len(triangle[i])-1:
-                    c2 = dp[i-1][j]
-                dp[i][j] = triangle[i][j] + min(c1, c2)
-        # print(dp)
-        return min(dp[-1])
+        # recursive
+#         def min_path_sum(row, col):
+#             if row > n or col > len(triangle[row]):
+#                 return float('inf')
+#             if row == n-1:
+#                 return triangle[row][col]
+            
+#             sum1 = min_path_sum(row+1, col)
+#             sum2 = min_path_sum(row+1, col+1)
+#             return triangle[row][col] + min(sum1, sum2)
+                        
+#         return min_path_sum(0,0)
     
-        # top-down solution
-        n = len(triangle)
-        dp = [[0]*(i+1) for i in range(n)]
-        
-        def solve(row, idx):
-            if row >= n:
-                return 0
-            if idx > len(triangle[row])-1:
-                return 0
+        # memoized
+        dp = [[-1]*(i+1) for i in range(n)]
+        def min_path_sum(row, col):
+            if row > n or col > len(triangle[row]):
+                return float('inf')
             if row == n-1:
-                return triangle[row][idx]
-            if dp[row][idx]:
-                return dp[row][idx]
-            
-            c1 = solve(row+1, idx)
-            c2 = solve(row+1, idx+1)
-            
-            dp[row][idx] = triangle[row][idx] + min(c1, c2)
-            return dp[row][idx]
-            
-        return solve(0, 0)
-        
+                return triangle[row][col]
+            if dp[row][col] != -1:
+                return dp[row][col]
+                
+            sum1 = min_path_sum(row+1, col)
+            sum2 = min_path_sum(row+1, col+1)
+            dp[row][col] = triangle[row][col] + min(sum1, sum2)
+            return dp[row][col]
+                        
+        return min_path_sum(0,0)
