7ad06bf1ce780b34b0af68855d4c6444290fde94
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-08-05 09:50:53 +0530

    Time: 63 ms (97.19%), Space: 17.9 MB (23.81%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -21,21 +21,21 @@ class Solution:
         # memoized
         # TC: 
         # SC: O(N^2) for dp array, O(N) for recursion heap stack space
-#         dp = [[-1]*(i+1) for i in range(n)]
-#         def min_path_sum(row, col):
-#             if row > n or col > len(triangle[row]):
-#                 return float('inf')
-#             if row == n-1:
-#                 return triangle[row][col]
-#             if dp[row][col] != -1:
-#                 return dp[row][col]
+        dp = [[-1]*(i+1) for i in range(n)]
+        def min_path_sum(row, col):
+            # if row > n or col > len(triangle[row]):
+            #     return float('inf')
+            if row == n-1:
+                return triangle[row][col]
+            if dp[row][col] != -1:
+                return dp[row][col]
                 
-#             sum1 = min_path_sum(row+1, col)
-#             sum2 = min_path_sum(row+1, col+1)
-#             dp[row][col] = triangle[row][col] + min(sum1, sum2)
-#             return dp[row][col]
+            sum1 = min_path_sum(row+1, col)
+            sum2 = min_path_sum(row+1, col+1)
+            dp[row][col] = triangle[row][col] + min(sum1, sum2)
+            return dp[row][col]
                         
-#         return min_path_sum(0,0)
+        return min_path_sum(0,0)
     
     
         # bottom-up
