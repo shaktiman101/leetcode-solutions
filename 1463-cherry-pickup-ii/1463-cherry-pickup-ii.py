06b80242067d0bd50396805dfd2ea05f4bf9fd38
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-08-07 21:31:21 +0530

    Time: 860 ms (95.26%), Space: 26.2 MB (58.16%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,101 +1,106 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        # bottom-up: tabulation
         m, n = len(grid), len(grid[0])
-        dp = [[[0]*n for _ in range(n)] for _ in range(m)]
-                    
-        for i in range(m-1, -1, -1):
-            for j in range(n):
-                for k in range(n):
-                    if i == m-1:
-                        dp[i][j][k] = grid[i][j]
-                        if j != k:
-                            dp[i][j][k] += grid[i][k]
-                    else: 
-                        c2,c3,c4,c5,c6,c7,c8,c9=[float('-inf')]*8
-                        c1 = dp[i+1][j][k]
-                        if k > 0:
-                            c2 = dp[i+1][j][k-1]
-                        if k < n-1:
-                            c3 = dp[i+1][j][k+1]
-
-                        if j > 0:
-                            c4 = dp[i+1][j-1][k]
-                            if k > 0:
-                                c5 = dp[i+1][j-1][k-1]
-                            if k < n-1:
-                                c6 = dp[i+1][j-1][k+1]
-
-                        if j < n-1:
-                            c7 = dp[i+1][j+1][k]
-                            if k > 0:
-                                c8 = dp[i+1][j+1][k-1]
-                            if k < n-1:
-                                c9 = dp[i+1][j+1][k+1]
-                        dp[i][j][k] = grid[i][j] + max(c1,c2,c3,c4,c5,c6,c7,c8,c9)
-                        if j != k:
-                            dp[i][j][k] += grid[i][k]        
-        return dp[0][0][n-1]
-        # print(dp)
         
-        # top-down memoization
-        m, n = len(grid), len(grid[0])
-        dp = [[[0]*n for _ in range(n)] for _ in range(m)]
+#         def func(left_robo, right_robo, grid):
+#             if left_robo[1]<0 or left_robo[1]>=n or right_robo[1]<0 or right_robo[1]>=n:
+#                 return 0
+#             if left_robo[0] == m-1:
+#                 return grid[left_robo[0]][left_robo[1]]
+#             if right_robo[0] == m-1:
+#                 return grid[right_robo[0]][right_robo[1]]
+            
+#             row, left_col = left_robo
+#             row, right_col = right_robo
+#             cherry1 = grid[row][left_col]
+#             cherry2 = grid[row][right_col]
+            
+#             grid[row][left_col] = 0
+#             c1 = cherry1 + func((row+1, left_col-1), right_robo, grid)
+#             c2 = cherry1 + func((row+1, left_col), right_robo, grid)
+#             c3 = cherry1 + func((row+1, left_col+1), right_robo, grid)
+#             left_robo_cherry = max(c1, c2, c3)
+            
+            
+            
+#             grid[row][right_col] = 0
+#             c4 = cherry2 + func(left_robo, (row+1, right_col-1), grid)
+#             c5 = cherry2 + func(left_robo, (row+1, right_col), grid)
+#             c6 = cherry2 + func(left_robo, (row+1, right_col+1), grid)
+#             right_robo_cherry = max(c4, c5, c6)
+            
+#             return left_robo_cherry + right_robo_cherry
         
-        def func(row, col1, col2):
-            if col1 < 0 or col1 >= n or col2 < 0 or col2 >= n:
-                return float('-inf')
+#         return func((0, 0), (0, n-1), grid) #+ func(0, n-1)
+
+        # recursive approach
+        # TC: O(9^(m*n))
+        # SC: O(N)
+#         def func(row, col1, col2, grid):
+#             if col1<0 or col1>=n or col2<0 or col2>=n:
+#                 return 0
+#             if row == m-1:
+#                 res = grid[row][col1]
+#                 if col1 != col2:
+#                     res += grid[row][col2]
+#                 return res
+            
+            
+#             c1 = func(row+1, col1-1, col2-1, grid)
+#             c2 = func(row+1, col1-1, col2, grid)
+#             c3 = func(row+1, col1-1, col2+1, grid)
+            
+#             c4 = func(row+1, col1, col2-1, grid)
+#             c5 = func(row+1, col1, col2, grid)
+#             c6 = func(row+1, col1, col2+1, grid)
+            
+#             c7 = func(row+1, col1+1, col2-1, grid)
+#             c8 = func(row+1, col1+1, col2, grid)
+#             c9 = func(row+1, col1+1, col2+1, grid)
+            
+#             res = grid[row][col1]
+#             if col1 != col2:
+#                 res += grid[row][col2]
+#             return  res + max(c1, c2, c3, c4, c5, c6, c7, c8, c9)
+        
+#         return func(0, 0, n-1, grid)
+    
+    
+        # memoized approach
+        # TC: 
+        # SC: 
+        dp = [[[-1]*n for _ in range(n)] for _ in range(m)]
+        def func(row, col1, col2, grid):
+            if col1<0 or col1>=n or col2<0 or col2>=n:
+                return 0
             if row == m-1:
-                if col1 == col2:
-                    return grid[row][col1]
-                return grid[row][col1]+grid[row][col2]
-            if dp[row][col1][col2]:
+                res = grid[row][col1]
+                if col1 != col2:
+                    res += grid[row][col2]
+                return res
+            if dp[row][col1][col2] != -1:
                 return dp[row][col1][col2]
-            c1 = func(row+1, col1, col2)
-            c2 = func(row+1, col1, col2-1)
-            c3 = func(row+1, col1, col2+1)
             
-            c4 = func(row+1, col1-1, col2)
-            c5 = func(row+1, col1-1, col2-1)
-            c6 = func(row+1, col1-1, col2+1)
+            c1 = func(row+1, col1-1, col2-1, grid)
+            c2 = func(row+1, col1-1, col2, grid)
+            c3 = func(row+1, col1-1, col2+1, grid)
             
-            c7 = func(row+1, col1+1, col2)
-            c8 = func(row+1, col1+1, col2-1)
-            c9 = func(row+1, col1+1, col2+1)
+            c4 = func(row+1, col1, col2-1, grid)
+            c5 = func(row+1, col1, col2, grid)
+            c6 = func(row+1, col1, col2+1, grid)
             
-            dp[row][col1][col2] = grid[row][col1] + max(c1,c2,c3,c4,c5,c6,c7,c8,c9)
+            c7 = func(row+1, col1+1, col2-1, grid)
+            c8 = func(row+1, col1+1, col2, grid)
+            c9 = func(row+1, col1+1, col2+1, grid)
+            
+            res = grid[row][col1]
             if col1 != col2:
-                dp[row][col1][col2] += grid[row][col2] 
+                res += grid[row][col2]
+            dp[row][col1][col2] = res + max(c1, c2, c3, c4, c5, c6, c7, c8, c9)
+            
             return dp[row][col1][col2]
         
-        z= func(0, 0, n-1)
-        print(dp)
-        return z
+        return func(0, 0, n-1, grid)
     
-        
-        # recursive solution
-        m, n = len(grid), len(grid[0])
-        def func(row, col1, col2):
-            if col1 < 0 or col1 >= n or col2 < 0 or col2 >= n or col1==col2:
-                return float('-inf')
-            if row == m-1:
-                return grid[row][col1]+grid[row][col2]
-            
-            c1 = func(row+1, col1, col2)
-            c2 = func(row+1, col1, col2-1)
-            c3 = func(row+1, col1, col2+1)
-            
-            c4 = func(row+1, col1-1, col2)
-            c5 = func(row+1, col1-1, col2-1)
-            c6 = func(row+1, col1-1, col2+1)
-            
-            c7 = func(row+1, col1+1, col2)
-            c8 = func(row+1, col1+1, col2-1)
-            c9 = func(row+1, col1+1, col2+1)
-            
-            return grid[row][col1] + grid[row][col2] + max(c1,c2,c3,c4,c5,c6,c7,c8,c9)
-        
-        return func(0, 0, n-1)
-            
-        
+    
         
