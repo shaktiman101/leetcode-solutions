35d9ac8946d6eb8c7c3ccb1d1c818d892fa2af4d
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-08-16 21:13:19 +0530

    Time: 1094 ms (28.98%), Space: 42.4 MB (35.83%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -27,11 +27,11 @@ class Solution:
         # memoized
         dp = [[-1]*(n+1) for _ in range(m+1)]
         def func(idx1, idx2):
-            if idx1 < 0 or idx2 < 0:
+            if idx1 < 1 or idx2 < 1:
                 return 0
             
-            if idx1 == 0 and idx2 == 0:
-                if text1[0] == text2[0]:
+            if idx1 == 1 and idx2 == 1:
+                if text1[idx1-1] == text2[idx2-1]:
                     return 1
                 return 0
             
@@ -39,7 +39,7 @@ class Solution:
                 return dp[idx1][idx2]
             
             c1, c2 = 0, 0
-            if text1[idx1] == text2[idx2]:
+            if text1[idx1-1] == text2[idx2-1]:
                 c1 = 1 + func(idx1-1, idx2-1)
             else:
                 c2 = max(func(idx1-1, idx2),func(idx1, idx2-1))
@@ -47,23 +47,24 @@ class Solution:
             dp[idx1][idx2] = max(c1, c2)
             return dp[idx1][idx2]
             
-        return func(m-1, n-1)
+        return func(m, n)
     
-    # tabulation
-#     dp = [[0]*(n+1) for _ in range(m+1)]
-#     # for i in range(m+1):
-        
     
-#     for i in range(1, m+1):
-#         for j in range(1, n+1):
-            
-#             c1, c2 = 0, 0
-#             if text1[i] == text2[j]:
-#                 c1 = 1 + dp[i-1][j-1]
-#             else:
-#                 c2 = max(dp[i-1][j], dp[i][j-1])
-                
-#             dp[i][j] = max(c1, c2)
-    
-#     return dp[m][n]
+        # tabulation
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        # for i in range(m+1):
+
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+
+                c1, c2 = 0, 0
+                if text1[i] == text2[j]:
+                    c1 = 1 + dp[i-1][j-1]
+                else:
+                    c2 = max(dp[i-1][j], dp[i][j-1])
+
+                dp[i][j] = max(c1, c2)
+
+        return dp[m][n]
             
