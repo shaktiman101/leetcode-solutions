56b8bd317b976b52687478898cdbadfc3aaba00f
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-08-16 19:49:48 +0530

    Time: 1154 ms (26.37%), Space: 42.5 MB (24.86%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,47 +1,50 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        #tabulation
+        
         m, n = len(text1), len(text2)
-        dp = [[0]*(n+1) for _ in range(m+1)]
-        
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                if text1[i-1]==text2[j-1]:
-                    ans = 1+dp[i-1][j-1]
-                else:
-                    ans = max(dp[i-1][j], dp[i][j-1])
-                dp[i][j] = ans
-        
-        return dp[m][n]
-        
-        # memoization
-        m, n = len(text1), len(text2)
-        dp = [[0]*n for _ in range(m)]
-        def func(m, n):
-            if m < 0 or n < 0:
-                return 0
-            if dp[m][n]:
-                return dp[m][n]
-            
-            if text1[m]==text2[n]:
-                ans = 1+func(m-1, n-1)
-            else:
-                ans = max(func(m-1, n), func(m, n-1))
-            dp[m][n] = ans
-            return ans
-        
-        func(m-1, n-1)
-        return dp[-1][-1]
-    
         
         # recursive
-        m, n = len(text1), len(text2)        
-        def func(m, n, subseq_len):
-            if m < 0 or n < 0:
+#         def func(idx1, idx2):
+#             if idx1 >= m or idx2 >= n:
+#                 return 0
+            
+#             if idx1 == m-1 and idx2 == n-1:
+#                 if text1[idx1] == text2[idx2]:
+#                     return 1
+#                 return 0
+            
+#             c1, c2 = 0, 0
+#             if text1[idx1] == text2[idx2]:
+#                 c1 = 1 + func(idx1+1, idx2+1)
+#             else:
+#                 c2 = max(func(idx1+1, idx2),func(idx1, idx2+1))
+            
+#             return max(c1, c2)
+            
+#         return func(0, 0)
+
+
+        # memoized
+        dp = [[-1]*(n+1) for _ in range(m+1)]
+        def func(idx1, idx2):
+            if idx1 >= m or idx2 >= n:
                 return 0
-            if text1[m]==text2[n]:
-                return 1+func(m-1, n-1, subseq_len+1)
+            
+            if idx1 == m-1 and idx2 == n-1:
+                if text1[idx1] == text2[idx2]:
+                    return 1
+                return 0
+            
+            if dp[idx1][idx2] != -1:
+                return dp[idx1][idx2]
+            
+            c1, c2 = 0, 0
+            if text1[idx1] == text2[idx2]:
+                c1 = 1 + func(idx1+1, idx2+1)
             else:
-                return max(func(m-1, n, subseq_len), func(m, n-1, subseq_len))
-        
-        return func(m-1, n-1, 0)
+                c2 = max(func(idx1+1, idx2),func(idx1, idx2+1))
+            
+            dp[idx1][idx2] = max(c1, c2)
+            return dp[idx1][idx2]
+            
+        return func(0, 0)
