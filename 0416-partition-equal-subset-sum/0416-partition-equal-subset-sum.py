994ccff2d502db340ee0d74e072410f97979c5ad
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2023-09-04 23:50:19 +0530

    Time: 2318 ms (21.98%), Space: 53.5 MB (24.18%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,40 +1,39 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
-        if total%2 == 1:
+        if total%2 != 0:
             return False
-        target = total//2
-        n = len(nums)
         
-        dp = [[-1]*(target+1) for _ in range(n+1)]
-        def func(n, target):
-            if target == 0:
-                if n == 0:
+        n = len(nums)
+#         def func(idx, target):
+#             if target < 0:
+#                 return False
+#             if idx==0:
+#                 if nums[0] == target or target == 0:
+#                     return True
+#                 return False
+            
+#             take = func(idx-1, target-nums[idx])
+#             not_take = func(idx-1, target)
+#             return take or not_take
+        
+#         return func(n-1, total//2)
+        target = total//2
+        dp = [[-1]*(target+1) for _ in range(n)]
+        def func(idx, target):
+            if target < 0:
+                return False
+            if idx==0:
+                if nums[0] == target or target == 0:
                     return True
                 return False
-            if target < 0 or n <= 0:
-                return False
-            if dp[n][target] != -1:
-                return dp[n][target]
+            if dp[idx][target] != -1:
+                return dp[idx][target]
             
-            dp[n][target] = func(n-1, target-nums[n-1]) or func(n-1, target)
-            return dp[n][target]
-        
-        return func(n, target)
-        
-        
-        
-        target = sum(nums)
-        if target%2 != 0:
-            return False
-        
-        n = len(nums)
-        def solve(n, target):
-            if target is 0:
-                return True
-            if n < 1 or target < 0:
-                return False
+            take = func(idx-1, target-nums[idx])
+            not_take = func(idx-1, target)
             
-            return solve(n-1, target-nums[n-1]) or \
-            solve(n-1, target)
-        return solve(n, target//2)
+            dp[idx][target] = take or not_take
+            return dp[idx][target]
+        
+        return func(n-1, target)
