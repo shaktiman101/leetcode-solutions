e08314e31dee367521db0380fef163146d4006e1
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2025-05-10 09:50:19 +0530

    Time: 22 ms (5.34%), Space: 17.8 MB (86.18%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,13 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        char_count = Counter(s)
+        for ch in t:
+            if not ch in char_count:
+                return False
+            char_count[ch] -= 1
+            if char_count[ch] == 0:
+                char_count.pop(ch)
+        if char_count:
+            return False
+        return True
