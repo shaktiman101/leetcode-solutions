90f871b5b6cdcebd9212cca7bdc6b539d65b81fb
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2025-05-11 22:32:31 +0530

    Time: 12 ms (60.49%), Space: 20.7 MB (76.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,24 +1,7 @@
+from collections import Counter, defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-            ans = {}
-            for word in strs:
-                sort_word = ''.join(sorted(word))
-                if sort_word in ans.keys():
-                    ans[sort_word].append(word)
-                else:
-                    ans[sort_word] = [word]
-                # print(ans)
-            return ans.values()
-            
-            hashmap = {}
-            for str_ in strs:
-                tmp = ''.join(sorted(str_))
-                if tmp in hashmap:
-                    hashmap[tmp].append(str_)
-                else:
-                    hashmap[tmp] = [str_]
-            
-            ans = []
-            for k in hashmap:
-                ans.append(hashmap[k])
-            return ans
+        result = defaultdict(list)
+        for word in strs:
+            result["".join(sorted(word))].append(word)
+        return list(result.values())
