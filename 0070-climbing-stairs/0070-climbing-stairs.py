7a9e49ab530748970fc813c2bf2b415e06f4976a
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2025-06-17 09:43:35 +0530

    Time: 0 ms (100%), Space: 17.6 MB (78.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,43 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # bottom up
-        dp = [-1]*(n+1)
-        dp[0] = dp[1] = 1
         
-        for i in range(2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
-    
-        # memoization - top down
-        dp = [-1]*(n+1)
-        def solve(n):
-            if n <= 1:
+        dp = [0]*(n+1)
+        def num_ways(n):
+            if n<=1 :
                 return 1
-            if dp[n] != -1:
+            if dp[n]:
                 return dp[n]
-            dp[n] = solve(n-1)+solve(n-2)
+            dp[n] =num_ways(n-1)+num_ways(n-2)
             return dp[n]
-        return solve(n)
-        
-#         n1, n2 = 1, 2
-#         if n < 2:
-#             return n1
-        
-#         for i in range(3, n+1):
-#             tmp = n2
-#             n2 = n1+n2
-#             n1 = tmp
-#         return n2
-#         mem = {}
-#         def solve(n):
-#             if n <= 2:
-#                 return n
-#             if mem.get(n, None):
-#                 return mem.get(n)
-#             n1 = solve(n-1)
-#             mem[n-1] = n1
-#             n2 = solve(n-2)
-#             mem[n-2] = n2
-#             return n1 + n2
-        
-#         return solve(n)
+
+        return num_ways(n)
