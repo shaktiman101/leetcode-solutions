197c8a2564d6e32adcb6795a6c1c8433e7eb4f37
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2025-06-22 22:42:15 +0530

    Time: 286 ms (62.89%), Space: 16.5 MB (100%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,34 +1,134 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        dp = [[""]*(n+1) for _ in range(n)]
+        if n == 1:
+            return s
 
-        def is_palindrome(left, right):
-            while left<right:
-                if s[left] != s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
+        # def is_palindrome(st_idx, end_idx):
+        #     if st_idx > end_idx:
+        #         return True
+        #     if s[st_idx] != s[end_idx]:
+        #         return False
+        #     return is_palindrome(st_idx+1, end_idx-1)
 
-        def func(left, right):
-            if left>=n or right<0 or left>right:
-                return ""
-            if left == right:
-                return s[left]
-            if dp[left][right]:
-                return dp[left][right]
+        # recursive
+        # def func(st_idx, end_idx, sub):
+        #     if st_idx > n-1 or end_idx < 0 or st_idx > end_idx:
+        #         return sub
+            
+        #     if is_palindrome(s, st_idx, end_idx) and end_idx-st_idx+1 > len(sub):
+        #         sub = s[st_idx:end_idx+1]
+        #         return func(st_idx+1, end_idx-1, sub)
+        #     else:
+        #         ans1 = func(st_idx+1, end_idx, sub)
+        #         ans2 = func(st_idx, end_idx-1, sub)
+        #         return ans1 if len(ans1) > len(ans2) else ans2
+            
+        # return func(0, n-1, "")
 
-            # if s[left] == s[right]:
-            #     dp[left][right] = s[left]+func(left+1, right-1)+s[left]
-            #     return dp[left][right]
-            if is_palindrome(left, right):
-                dp[left][right] = s[left: right+1]
-                return dp[left][right]
-            else:
-                s1 = func(left+1, right)
-                s2 = func(left, right-1)
-                dp[left][right] = s1 if len(s1) > len(s2) else s2
-                return dp[left][right]
+        # memoization
+        # dp = [[-1]*(n+1) for _ in range(n+1)]
+        # def func(st_idx, end_idx, sub):
+        #     if st_idx > n-1 or end_idx < 0 or st_idx > end_idx:
+        #         return sub
+            
+        #     if dp[st_idx][end_idx] != -1:
+        #         return dp[st_idx][end_idx]
 
-        return func(0, n-1)
+        #     if is_palindrome(s, st_idx, end_idx) and end_idx-st_idx+1 > len(sub):
+        #         sub = s[st_idx:end_idx+1]
+        #         dp[st_idx][end_idx] = func(st_idx+1, end_idx-1, sub)
+        #         return dp[st_idx][end_idx]
+        #     else:
+        #         ans1 = func(st_idx+1, end_idx, sub)
+        #         ans2 = func(st_idx, end_idx-1, sub)
+        #         dp[st_idx][end_idx] = ans1 if len(ans1) > len(ans2) else ans2
+        #         return dp[st_idx][end_idx]
+            
+        # return func(0, n-1, "")
+
+
+        # tabulation
+        # dp = [[""]*(n+1) for _ in range(n+1)]
+
+        # sub = ""
+        # for st_idx in range(n):
+        #     for end_idx in range(st_idx+1, n):
+        #         if is_palindrome(st_idx, end_idx) and end_idx-st_idx+1 > len(sub):
+        #             sub = s[st_idx:end_idx+1]
+        #             dp[st_idx][end_idx] = sub   #dp[st_idx-1][end_idx-1]
+        #         else:
+        #             ans1 = dp[st_idx-1][end_idx]
+        #             ans2 = dp[st_idx][end_idx-1]
+        #             dp[st_idx][end_idx] = ans1 if len(ans1) > len(ans2) else ans2
+        
+        # return dp[n][n]
+
+        # recursive 
+        # s2 = s[::-1]
+        # longest_sub = ""
+
+        # def func(idx1, idx2, curr_longest):
+        #     nonlocal longest_sub
+
+        #     if idx1 < 0 or idx2 < 0:
+        #         return
+
+        #     if s[idx1] == s2[idx2]:
+        #         func(idx1-1, idx2-1, curr_longest+s[idx1])
+        #     else:
+        #         longest_sub = curr_longest if len(curr_longest) > len(longest_sub) else longest_sub
+        #         func(idx1-1, idx2, "")
+        #         func(idx1, idx2-1, "")
+
+
+        # func(n-1, n-1, "")
+        # return longest_sub
+
+
+        # s2 = s[::-1]
+        # longest_sub = ""
+        # dp =[[""]*(n+1) for _ in range(n+1)]
+
+        # for idx1 in range(1,n+1):
+        #     for idx2 in range(1,n+1):
+        #         if s[idx1-1] == s2[idx2-1]:
+        #             dp[idx1][idx2] = dp[idx1-1][idx2-1] + s[idx1-1]
+        #             longest_sub = dp[idx1][idx2] if len(dp[idx1][idx2]) > len(longest_sub) else longest_sub
+        #         # else:
+                    
+        #             # dp[idx1][idx2] = dp[idx1-1][idx2]
+        #             # dp[idx1][idx2] = dp[idx1][idx2-1]
+        # print(dp)
+        # return longest_sub
+
+        n = len(s)
+        def palindrome_at(st_idx, end_idx):
+            palin_st, palin_end = st_idx, st_idx
+
+            while st_idx>=0 and end_idx<n and s[st_idx]==s[end_idx]:
+                palin_st, palin_end = st_idx, end_idx
+
+                st_idx -= 1
+                end_idx += 1
+            return palin_st, palin_end
+            
+            
+
+        palin_st, palin_end = 0, 0
+        for center in range(n):
+            # check for odd length substring
+            odd = palindrome_at(center, center)
+            if odd[1]-odd[0] > palin_end-palin_st:
+                palin_st, palin_end = odd
+
+            # check for even length substring
+            even = palindrome_at(center, center+1)
+            if even[1]-even[0] > palin_end-palin_st:
+                palin_st, palin_end = even
+
+        return s[palin_st:palin_end+1]
+
+
+
+            
