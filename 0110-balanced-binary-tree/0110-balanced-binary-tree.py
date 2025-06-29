430f5728e8aabcc8e2ba6688ccb13819c878c352
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2025-06-29 22:30:03 +0530

    Time: 0 ms (100%), Space: 19 MB (26.88%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -6,42 +6,25 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if not root:
-            return True
         
-        def func(root):
+        def height(root):
             if not root:
                 return 0
-            lh = func(root.left)
-            if isinstance(lh, bool):
+
+            l = height(root.left)
+            if isinstance(l, bool):
+                return l
+            
+            r = height(root.right)
+            if isinstance(r, bool):
+                return r
+
+            if abs(l-r) > 1:
                 return False
-            rh = func(root.right)
-            if isinstance(rh, bool):
-                return False
-            if abs(lh-rh) > 1:
-                return False
-            return max(lh, rh)+1
-        return func(root)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        def func(root):
-            if not root:
-                return 0
-            height_left = 1+func(root.left)
-            height_right = 1+func(root.right)
-            if isinstance(height_left, int) and isinstance(height_left, int) and height_left != height_right:
-                return False
-            else:
-                return height_left
-        return func(root)
-        
+            return 1+max(l, r)
+
+        ans = height(root)
+        if isinstance(ans, bool):
+            return ans
+
+        return True
