8b68c6d7f10d98c405af59576a1eccdd742b1939
shaktiman101 <42404233+shaktiman101@users.noreply.github.com>
2025-07-20 23:05:19 +0530

    Time: 34 ms (0.66%), Space: 16.5 MB (100%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,28 +5,53 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        nodes = 0
-        cur = head
-        while cur:
-            nodes += 1
-            cur = cur.next
-        # if nodes == 1:
+        # node_count = 0
+        # curr = head
+
+        # while curr:
+        #     curr = curr.next
+        #     node_count += 1
+
+        # target_node = node_count-n+1
+        # curr = head
+        # prev = None
+
+        # while target_node != 1:
+        #     prev = curr
+        #     curr = curr.next
+        #     target_node -= 1
+        
+        # if prev is None:
         #     return head.next
+        # prev.next = curr.next
+        # return head
 
-        node_from_beginning = nodes-n
+        # dummy = ListNode(0, head)
+        # left = dummy
+        # right = head
 
-        cur = head
-        prev = cur
-        cur = cur.next
-        count = 1
-        if not cur or node_from_beginning==0:
-            return cur
+        # while n > 0:
+        #     right = right.next
+        #     n -= 1
+        
+        # while right:
+        #     right = right.next
+        #     left = left.next
 
-        while cur:
-            if count == node_from_beginning:
-                prev.next = cur.next
-            prev = cur
-            cur = cur.next
-            count += 1
+        # left.next = left.next.next
+        # return dummy.next
 
+        left, right = head, head
+        for _ in range(n):
+            right = right.next
+
+        if not right:
+            return head.next
+
+        while right.next:
+            left = left.next
+            right = right.next
+        left.next = left.next.next
         return head
+
+
